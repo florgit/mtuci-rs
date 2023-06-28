b862d39eedc7db513bbef0a561a11705c5fc1d3c
andrey <andreygabovv@gmail.com>
2023-06-29 00:55:40 +0300

    modified homework

--- homework_1/src/main.rs
+++ homework_1/src/main.rs
@@ -5,9 +5,9 @@ fn main() {
         } else if i % 3 == 0 {
             println!("Fizz");
         } else if i % 5 == 0 {
-            println!("Buzz")
+            println!("Buzz");
         } else {
-            println!("{i}")
+            println!("{i}");
         }
     }
 }
